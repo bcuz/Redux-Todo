b0d303358b65fcd113e16de2f8e051621764cd9e
adam <bcuz@users.noreply.github.com>
2019-04-10 22:27:18 -0400

    input state

--- src/App.js
+++ src/App.js
@@ -3,11 +3,23 @@ import './App.css';
 import { connect } from 'react-redux'
 
 class App extends Component {
+  state = {
+    input: ''
+  }
+
+  handleInputChange = e => {
+    this.setState({
+      input: e.target.value
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        {this.props.todos.length}
-      </div>
+      <form>
+        <input type="text" onChange={this.handleInputChange} value={this.state.input} />
+        <input type="submit" value="Send"/>
+        <ul></ul>
+      </form>
     );
   }
 }
